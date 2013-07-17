d2c3b404394f69985b5e1d220535d44af80e2b0c
J. Leary <info@periodtech.com>
2013-07-17 16:50:35 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PeriodTech::Application.config.secret_token = '9faeb0c4af0340723eee3019d05b051d1a95b8353ba035b6ce3d821354b2579019d4c43bed528609e46ec100e43d6fe1754d6daebf5e46064eeed8e506afdfe5'
